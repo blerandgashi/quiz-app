0d2c4a6ce054ef0ddabab1a693fee08d9bd9e18a
Blerand Gashi <blerandgashi15@gmail.com>
2025-11-18 21:37:40 +0100

    done with the project!

--- script.js
+++ script.js
@@ -51,5 +51,101 @@ const questions = [
   }
 ];
 
-const currentquestionIndex = 0;
-const score = 0;
+let currentquestionIndex = 0;
+let score = 0;
+
+let startScreenEl = document.querySelector("#start-screen");
+let quizScreenEl = document.querySelector("#quiz-screen");
+let questionText = document.querySelector("#question-text");
+
+const startBtn = document.querySelector("#start-btn");
+
+startBtn.addEventListener("click", function(){
+  startScreenEl.classList.add("hidden");
+  quizScreenEl.classList.remove("hidden");
+  getQuestions()
+})
+
+function getQuestions(){
+  let question = questions[currentquestionIndex]
+  questionText.textContent = question.question;
+
+  let questionNumber = document.querySelector("#question-number");
+  questionNumber.textContent = `Question ${currentquestionIndex + 1} of ${questions.length}`
+
+  let answersContainer = document.querySelector("#answers");
+  const answersArray = question.answers;
+
+  answersContainer.innerHTML = ""
+
+  answersArray.forEach((answers, index) => {
+    const btns = document.createElement("button");
+    btns.textContent = answers;
+    btns.classList.add("answer-btn")
+
+    btns.addEventListener("click", function(){
+      checkAnswer(index)    
+    })
+    answersContainer.appendChild(btns);
+  })
+}
+
+function checkAnswer(correctAnswerIndex){
+  const allBtns = document.querySelectorAll(".answer-btn");
+  const nextBtn = document.querySelector("#next-btn");
+
+  allBtns.forEach(currentBtn =>{
+    currentBtn.disabled = true;
+  })
+  if (questions[currentquestionIndex].correct === correctAnswerIndex) {
+    allBtns[correctAnswerIndex].classList.add("correct");
+    score++;
+    document.querySelector("#score").textContent = `${score} of ${questions.length}`;
+  }else{
+    allBtns[correctAnswerIndex].classList.add("wrong");
+    allBtns[questions[currentquestionIndex].correct].classList.add("correct");
+  }
+  nextBtn.classList.remove("hidden");
+}
+
+const nextBtn = document.querySelector("#next-btn")
+nextBtn.addEventListener("click", function(){
+  currentquestionIndex++;
+  if (currentquestionIndex < questions.length) {
+    getQuestions();
+    nextBtn.classList.add("hidden")
+  }else{
+    showResult()
+  }
+})
+
+function showResult(){
+  document.querySelector("#quiz-screen").classList.add("hidden");
+  document.querySelector("#next-btn").classList.add("hidden");
+
+  document.querySelector("#results-screen").classList.remove("hidden");
+
+  const finalScore = document.querySelector("#final-score").textContent = score;
+  document.querySelector(".final-score").textContent = `Your score: ${finalScore} / ${questions.length}`
+
+  const resultMessage = document.querySelector("#result-message");
+
+  if (score === questions.length) {
+    resultMessage.textContent = "Excellent knowledge!"
+  }else if (score >= 3) {
+    resultMessage.textContent = "Good job there!" 
+  }else{
+    resultMessage.textContent = "You go step by step!"
+  }
+}
+
+const restartBtn = document.querySelector("#restart-btn");
+
+restartBtn.addEventListener("click", function(){
+  document.querySelector("#results-screen").classList.add("hidden");
+  document.querySelector("#start-screen").classList.remove("hidden");
+
+  score = 0;
+  document.querySelector("#score").textContent = score;
+  currentquestionIndex = 0;
+})
